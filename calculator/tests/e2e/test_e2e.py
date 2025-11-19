581be9787f971cbb5f68ff29315f7aab003ec104
joaaovinicius <110140140+joaaovinicius@users.noreply.github.com>
2025-11-18 21:22:37 -0300

    E agora?

--- calculator/tests/e2e/test_e2e.py
+++ calculator/tests/e2e/test_e2e.py
@@ -1,14 +1,17 @@
 from playwright.sync_api import sync_playwright
 import time
+import os
 
 def test_frontend_e2e():
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
 
-        page.goto("file:///C:/Projetos/TC-JOGO/Codigo-Calculadora/calculator/frontend/index.html")
+        # Caminho correto para funcionar no Windows e no GitHub Actions (Linux)
+        caminho = os.path.abspath("calculator/frontend/index.html")
+        page.goto(f"file:///{caminho.replace(os.sep, '/')}")
 
-        # Clicar usando o atributo onclick (mais seguro)
+        # Clicar usando os atributos onclick
         page.click("button[onclick='appendNumber(2)']")
         page.click("button[onclick=\"setOperation('+')\"]")
         page.click("button[onclick='appendNumber(3)']")
